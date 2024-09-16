1d9986b83959ff1ec39596415d1056f38c929ad1
felipe-tomino <felipe.tomino@gmail.com>
2024-09-16 20:35:49 -0300

    backend port as env var

--- backend/index.mjs
+++ backend/index.mjs
@@ -5,7 +5,7 @@ import knex from 'knex';
 import knexfile from './knexfile.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
