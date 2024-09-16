03b9c883783d1bff3eb0ca4c14c92e7a58aa2cde
felipe-tomino <felipe.tomino@gmail.com>
2024-09-16 20:55:09 -0300

    ssl reject unauthorized

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -42,13 +42,13 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      connectionString: process.env.DB_URL,
-      // database: process.env.DB_NAME,
-      // user:     process.env.DB_USER,
-      // password: process.env.DB_PASSWORD,
-      // host: process.env.DB_HOST,
-      // port: process.env.DB_PORT || 5432,
-      // ssl: true
+      // connectionString: process.env.DB_URL,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT || 5432,
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
